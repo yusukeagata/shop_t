7a939ffcc6878572773864182b6d47c80f167730
yusukeagata <yusuke.agata1013@gmail.com>
2016-10-23 16:32:30 +0000

    1024

--- config/application.rb
+++ config/application.rb
@@ -39,9 +39,10 @@ module ShopT
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    #config.i18n.load_path += Dir[Rails.root.join('config', 'locales','**', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    #  config.i18n.default_locale = :ja
+    config.i18n.default_locale = :ja
+    config.action_view.raise_on_missing_translations = false
 
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
